8daeb69c77eb0cba001fc41935a250714906f8e9
Riudiu <lacvert13@gmial.com>
2023-04-05 17:45:40 +0900

    int 치환 문제 해결, 모임 리스트 조회 api 추가

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ client = MongoClient('localhost', 27017)
 db = client.jungle  
 
 class Util:
-    # auto increment
+    # auto increment - idx
     def get_next_sequence(collection_name, field_name):
         result = db['counters'].find_one_and_update(
             {'_id': collection_name},
@@ -20,6 +20,11 @@ class Util:
             return "https://img.freepik.com/premium-photo/vertical-shot-soccer-ball-with-player-s-legs-waiting-kick-it_755989-104.jpg?w=2000"
         else:
             return "https://cdn.huffingtonpost.kr/news/photo/201512/18818_36342.jpeg"
+
+    # 유저 반 정보 가져오기
+    def getHostUserClass(userIdx):
+        result = db['users'].find_one({'user_idx': userIdx})
+        return result['user_class']
         
         
 
