ed40fef9cca27eb8dfca58b33dbc838e0f137927
Riudiu <lacvert13@gmial.com>
2023-04-06 02:46:42 +0900

    카테고리 이미지 추가

--- util.py
+++ util.py
@@ -16,8 +16,18 @@ class Util:
 
     # 카테고리 이미지 가져오기
     def getCategoryImageUrl(value):
-        if value == 'soccer':
-            return "https://img.freepik.com/premium-photo/vertical-shot-soccer-ball-with-player-s-legs-waiting-kick-it_755989-104.jpg?w=2000"
+        if value == 'football':
+            return "https://image.fmkorea.com/files/attach/new/20181201/33854530/577002901/1420290663/b1830cd0a8207833c6baf5bacf2df039.jpg"
+        if value == 'running':
+            return "https://thumbor.bigedition.com/img/4S_0eyjdLZhEQo6lJjijIThz5P0=/0x0:1000x1500/480x720/filters:quality(80)/granite-web-prod/35/6f/356fb6960a1444ca839435c4ae20a0e2.jpeg"
+        if value == 'walk':
+            return "https://static01.nyt.com/images/2022/08/03/well/FITNESS-WALKING-MEALS/FITNESS-WALKING-MEALS-videoSixteenByNine3000.jpg"
+        if value == 'basketball':
+            return "https://nypost.com/wp-content/uploads/sites/2/2023/03/Sean-Bairstow-1-copy.jpg"
+        if value == 'badminton':
+            return "https://badminton-coach.co.uk/wp-content/uploads/2013/04/badminton-jump-smash2.jpg"
+        if value == 'weight':
+            return "https://i.imgflip.com/1y0ge4.jpg"
         else:
             return "https://cdn.huffingtonpost.kr/news/photo/201512/18818_36342.jpeg"
 
